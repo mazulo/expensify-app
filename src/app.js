e4c69eb62f089eaf1c535da010d65a07dbbf8775
Patrick Mazulo <pmazulo@gmail.com>
2018-07-20 19:28:35 -0300

    Add react-router-dom and start implement it

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import 'normalize.css/normalize.css';
 
 import './styles/styles.scss';
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={} exact={ true } />
+      <Route path="/create" component={} />
+      <Route path="/edit" component={} />
+      <Route path="/help" component={} />
+      <Route component={} />
+    </Switch>
+  </BrowserRouter>
+);
 
 ReactDOM.render(<p>My boilerplate</p>, document.getElementById('app'))
