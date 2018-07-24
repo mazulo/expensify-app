044122303cccc4a37155b9a4a3896f779d983807
Patrick Mazulo <pmazulo@gmail.com>
2018-07-23 23:51:10 -0300

    Split the project into separated component and router files

--- src/app.js
+++ src/app.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import 'normalize.css/normalize.css';
 
 import './styles/styles.scss';
+import AppRouter from './routers/AppRouter';
 
-const routes = (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" component={} exact={ true } />
-      <Route path="/create" component={} />
-      <Route path="/edit" component={} />
-      <Route path="/help" component={} />
-      <Route component={} />
-    </Switch>
-  </BrowserRouter>
-);
 
-ReactDOM.render(<p>My boilerplate</p>, document.getElementById('app'))
+ReactDOM.render(<AppRouter />, document.getElementById('app'))
