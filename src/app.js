bae65934dcb06ce19352444e9dfaaf2eba8b93f4
Patrick Mazulo <pmazulo@gmail.com>
2018-09-03 14:15:34 -0300

    connecting store and component with react-redux

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import 'normalize.css/normalize.css';
 
 import './styles/styles.scss';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
 
+const store = configureStore();
+console.log(store.getState());
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+const jsx = (
+    <Provider store={ store }>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'))
