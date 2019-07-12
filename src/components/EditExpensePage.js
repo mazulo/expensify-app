79466157d79b405c371820cdc482983bc583c4d4
Patrick Mazulo <pmazulo@gmail.com>
2019-07-11 23:12:12 -0300

    Create link between expense list item and expense edit page

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 
 const EditExpensePage = (props) => {
   console.log(props);
   return (
     <div>
-      Editing the expense with id of { props.match.params.id }
+      Editing the expense with id of { props.expense.id }
     </div>
   );
 };
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find(
+      (expense) => expense.id === props.match.params.id
+    )
+  }
+}
+
+export default connect(mapStateToProps)(EditExpensePage);
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ description, amount, createdAt, id, dispatch }) => {
+const ExpenseListItem = ({
+    description,
+    amount,
+    createdAt,
+    id,
+    dispatch,
+    history,
+}) => {
     console.log(dispatch)
     return (
         <div>
-            <h3>{ description }</h3>
+            <h3>
+                <Link
+                    to={`/edit/${id}`}
+                >
+                    { description }
+                </Link>
+            </h3>
             <p>{ amount } - { createdAt }</p>
             <button
                 onClick={ () => dispatch(removeExpense({ id })) }
